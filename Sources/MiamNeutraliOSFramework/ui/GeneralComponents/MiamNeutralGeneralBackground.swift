0710271d520611ebcf5339911808f3561aac4920
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-08-09 14:31:09 +0100

    Added Background to CatalogView, made it easy to have defaults for Views

--- Sources/MiamNeutraliOSFramework/ui/GeneralComponents/MiamNeutralGeneralBackground.swift
+++ Sources/MiamNeutraliOSFramework/ui/GeneralComponents/MiamNeutralGeneralBackground.swift
@@ -1,5 +1,5 @@
 //
-//  MiamNeutralEmptyBackground.swift
+//  MiamNeutralGeneralBackground.swift
 //  
 //
 //  Created by didi on 09/08/2023.
@@ -9,7 +9,7 @@ import SwiftUI
 import MiamIOSFramework
 
 @available(iOS 14, *)
-public struct MiamNeutralEmptyBackground: GeneralBackgroundViewTemplate {
+public struct MiamNeutralGeneralBackground: GeneralBackgroundViewTemplate {
     public init() {}
     public func content() -> some View {
         HStack {}
@@ -17,8 +17,8 @@ public struct MiamNeutralEmptyBackground: GeneralBackgroundViewTemplate {
 }
 
 @available(iOS 14, *)
-struct MiamNeutralEmptyBackground_Previews: PreviewProvider {
+struct MiamNeutralGeneralBackground_Previews: PreviewProvider {
     static var previews: some View {
-        MiamNeutralEmptyBackground().content()
+        MiamNeutralGeneralBackground().content()
     }
 }
