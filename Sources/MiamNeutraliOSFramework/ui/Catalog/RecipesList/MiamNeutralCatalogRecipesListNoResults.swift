dc2324ece2de67ac233075f26770170d1d5e29fa
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-08-10 16:24:08 +0100

    added CatalogRecipesLists items

---
+++ Sources/MiamNeutraliOSFramework/ui/Catalog/RecipesList/MiamNeutralCatalogRecipesListNoResults.swift
@@ -0,0 +1,62 @@
+//
+//  MiamNeutralCatalogRecipesListNoResults.swift
+//  
+//
+//  Created by didi on 10/08/2023.
+//
+
+import SwiftUI
+import MiamIOSFramework
+import miamCore
+
+@available(iOS 14, *)
+public struct MiamNeutralCatalogRecipesListNoResults: CatalogRecipesListNoResultsViewTemplate {
+    let searchString: String = FilterViewModelInstance.shared.instance.currentState.searchString ?? ""
+    public init() {}
+    public func content(
+        showingFavorites: Bool,
+        browseCatalogAction: @escaping () -> Void
+    ) -> some View {
+        VStack(spacing: 32.0) {
+            Image.miamImage(icon: .noResults)
+
+            if showingFavorites {
+                Text(Localization.catalog.preferencesNoResult.localised)
+//                Text(MiamText.sharedInstance.noFavoritRecipeYet)
+                    .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleBigStyle)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.miamColor(.white))
+                Button {
+                        browseCatalogAction()
+                } label: {
+                    Text(Localization.catalog.browseRecipes.localised)
+                        .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyBigBoldStyle)
+                        .foregroundColor(Color.miamColor(.primary))
+                    Image.miamImage(icon: .rightArrow)
+                }
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .background(Capsule().foregroundColor(.white))
+                .overlay(Capsule().stroke(.white, lineWidth: 1.0))
+            } else {
+                Text("\(Localization.catalog.noRecipeFound.localised) \"\(searchString)\"")
+                    .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleBigStyle)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.miamColor(.white))
+                
+                Text(Localization.catalog.tryAnotherSearch.localised)
+                    .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyBigStyle)
+                    .foregroundColor(Color.miamColor(.white))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(10)
+        .background(Color.miamColor(.primaryDark))
+    }
+}
+
+@available(iOS 14, *)
+struct MiamNeutralCatalogRecipesListNoResults_Previews: PreviewProvider {
+    static var previews: some View {
+        MiamNeutralCatalogRecipesListNoResults().content(showingFavorites: false, browseCatalogAction: {})
+    }
+}
---
+++ Sources/MiamNeutraliOSFramework/ui/Catalog/RecipesList/MiamNeutralCatalogRecipesListTitle.swift
@@ -0,0 +1,30 @@
+//
+//  MiamNeutralCatalogRecipesListTitle.swift
+//  
+//
+//  Created by didi on 10/08/2023.
+//
+
+import SwiftUI
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct MiamNeutralCatalogRecipesListTitle: CatalogRecipesListTitleViewTemplate {
+    public init() {}
+    public func content(title: String, subtitle: String) -> some View {
+        HStack {
+            Text(title)
+                .foregroundColor(Color.blue)
+                .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleStyle)
+                .frame(height: 40.0)
+            Spacer()
+        }
+    }
+}
+
+@available(iOS 14, *)
+struct MiamNeutralCatalogRecipesListTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        MiamNeutralCatalogRecipesListTitle().content(title: "Test", subtitle: "Subtitle")
+    }
+}
