833b270e7b401ddef3327c77864d7897f13d00a6
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-09-21 14:29:54 +0100

    add Search

---
+++ Sources/MiamNeutraliOSFramework/ui/GeneralComponents/MiamNeutralGeneralSearch.swift
@@ -0,0 +1,50 @@
+//
+//  MiamNeutralGeneralSearch.swift
+//  
+//
+//  Created by didi on 21/09/2023.
+//
+
+import SwiftUI
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct MiamNeutralGeneralSearch: GeneralSearchViewTemplate {
+    public init() {}
+    public func content(
+        searchText: Binding<String>,
+        apply: @escaping () -> Void
+    ) -> some View {
+        VStack(spacing: 10.0) {
+            HStack(spacing: 10.0) {
+                HStack(spacing: 10.0) {
+                    TextField(Localization.catalog.searchTitle.localised, text: searchText).frame(height: 45.0)
+                        .disableAutocorrection(true)
+                    Button {
+                        apply()
+                    } label: {
+                        Image.miamImage(icon: .search)
+                            .renderingMode(.template)
+                            .foregroundColor(Color.miamColor(.white))
+                            .padding(10)
+                            .background(Color.miamColor(.primary)).clipShape(Circle())
+                            .shadow(radius: 2.0)
+                    }
+                }
+                .padding([.leading], 16).frame(height: 45.0)
+                .padding([.trailing], 2)
+                .overlay(Capsule().stroke(Color.gray, lineWidth: 1.0))
+            }.padding(10)
+            Spacer()
+        }
+    }
+}
+
+@available(iOS 14, *)
+struct MiamNeutralGeneralSearch_Previews: PreviewProvider {
+    static var previews: some View {
+        MiamNeutralGeneralSearch().content(
+            searchText: .constant(""),
+            apply: {})
+    }
+}
