c7b4c0878735adf15c45e25fca79c7f49d1ff8aa
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-10-02 15:07:14 +0200

    add defaults for BasketRecipes

--- Sources/MiamNeutraliOSFramework/DefaultPageParameters/CatalogFilterViewParameters.swift
+++ Sources/MiamNeutraliOSFramework/DefaultPageParameters/CatalogFilterViewParameters.swift
@@ -5,10 +5,8 @@
 //  Created by didi on 29/09/2023.
 //
 
-import UIKit
 import SwiftUI
 import MiamIOSFramework
-import miamCore
 
 @available(iOS 14, *)
 extension CatalogFilterViewParameters {
---
+++ Sources/MiamNeutraliOSFramework/ui/BasketRecipe/MiamNeutralBasketProduct.swift
@@ -0,0 +1,125 @@
+//
+//  MiamNeutralBasketProduct.swift
+//  
+//
+//  Created by didi on 02/10/2023.
+//
+
+import SwiftUI
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct MiamNeutralBasketProduct: BasketProductViewTemplate {
+    
+    public init() {}
+    public func content(
+        quantity: Binding<Int>,
+        infos: BasketProductInfos,
+        actions: BasketProductActions
+    ) -> some View {
+        VStack(alignment: .leading) {
+            HStack(spacing: Dimension.sharedInstance.lPadding) {
+                AsyncImage(url: infos.pictureURL) { image in
+                    image.resizable()
+                }
+                .frame(width: 96, height: 96)
+                infoAndTitle(
+                    name: infos.name,
+                    description: infos.description,
+                    delete: actions.delete
+                )
+            }
+            .frame(maxWidth: .infinity)
+            HStack {
+                priceContent(
+                    price: infos.price.price,
+                    pricePerUnit: infos.pricePerUnit.price
+                )
+                Spacer()
+                HStack(spacing: 12.0) {
+                    if infos.isSubstitutable {
+                        changeProductButton(
+                            changeProduct: actions.changeProduct
+                        )
+                    }
+                    MiamNeutralStepper(value: quantity)
+                        .frame(width: 140.0)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    func infoAndTitle(
+        name: String,
+        description: String,
+        delete: @escaping () -> Void
+    ) -> some View {
+            return HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: Dimension.sharedInstance.sPadding) {
+                    Text(name)
+                        .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyStyle)
+                    Text(description)
+                        .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyStyle)
+                    // TODO: add line with quantity?
+                }
+                Spacer()
+                Button {
+                    delete()
+                } label: {
+                    Image.miamNeutralImage(icon: .bin)
+                }
+                .padding(Dimension.sharedInstance.mlPadding)
+        }.frame(maxWidth: .infinity)
+    }
+    
+    func priceContent(price: Double, pricePerUnit: Double) -> some View {
+        return  VStack {
+            Text(price.currencyFormatted)
+                .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyBigBoldStyle)
+                .foregroundColor(Color.miamNeutralColor(.primary))
+            Text(formatPricePerUnit(pricePerUnit: price, unit: Localization.price.currency.localised))
+                .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodySmallStyle)
+        }
+    }
+    
+    func changeProductButton(changeProduct: @escaping () -> Void) -> some View {
+        return Button {
+            changeProduct()
+        } label: {
+            HStack {
+                Image.miamNeutralImage(icon: .refresh)
+                Text("Remplacer")
+                    .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyMediumBoldStyle)
+                    .foregroundColor(Color.miamNeutralColor(.primary))
+            }
+        }
+    }
+
+    func formatPricePerUnit(pricePerUnit: Double, unit: String) -> String {
+        return "\(pricePerUnit) / \(unit)"
+    }
+}
+
+@available(iOS 14, *)
+struct MiamNeutralBasketProduct_Previews: PreviewProvider {
+    static var previews: some View {
+        let infos = BasketProductInfos(
+            price: Price(price: 34.0, currency: "EUR"),
+            name: "Test",
+            description: "MY description",
+            pictureURL: (URL(string: "https://picsum.photos/400/300") ?? URL(string: ""))!,
+            sharedRecipeCount: 3,
+            isSubstitutable: false,
+            pricePerUnit: Price(price: 56.0, currency: "EUR"),
+            isLoading: false)
+        let actions = BasketProductActions(
+            delete: {},
+            changeProduct: {},
+            updateGuests: { _ in })
+        MiamNeutralBasketProduct().content(
+            quantity: .constant(4),
+            infos: infos,
+            actions: actions)
+    }
+}
