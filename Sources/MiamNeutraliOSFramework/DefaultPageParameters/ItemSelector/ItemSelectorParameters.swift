2fabded350b01b0a5e996844cd3c748ac512027b
Thibault Corbrion <thibault.corbrion@miam.tech>
2023-10-12 11:07:33 +0200

    add viewoptions into iteme selector

--- Sources/MiamNeutraliOSFramework/DefaultPageParameters/ItemSelector/ItemSelectorParameters.swift
+++ Sources/MiamNeutraliOSFramework/DefaultPageParameters/ItemSelector/ItemSelectorParameters.swift
@@ -13,20 +13,47 @@ import MiamIOSFramework
 /// This sets the Templates for the ItemSelector
 public struct ItemSelectorParameters: ItemSelectorParametersProtocol {
     
-    
     public var selectedProduct: TypeSafeItemSelectorSelectedProduct
     public var productOptions: TypeSafeItemSelectorOptionProducts
+    public var background: TypeSafeBackground
+    public var empty: TypeSafeEmpty
+    public var loading: TypeSafeLoading
     public var onItemSelected: () -> Void
     
     public init(
         onItemSelected: @escaping () -> Void,
+        viewOptions : ItemSelectorViewOptions
+    ){
+        self.selectedProduct = viewOptions.selectedProduct
+        self.productOptions = viewOptions.productOptions
+        self.background = viewOptions.background
+        self.empty = viewOptions.empty
+        self.loading = viewOptions.loading
+        self.onItemSelected = onItemSelected
+    }
+}
+
+@available(iOS 14, *)
+public struct ItemSelectorViewOptions {
+    
+    public var selectedProduct: TypeSafeItemSelectorSelectedProduct
+    public var productOptions: TypeSafeItemSelectorOptionProducts
+    public var background: TypeSafeBackground
+    public var empty: TypeSafeEmpty
+    public var loading: TypeSafeLoading
+    
+    public init(
         selectedProduct: TypeSafeItemSelectorSelectedProduct = TypeSafeItemSelectorSelectedProduct(MiamNeutralItemSelectorSelectedProduct()),
         productOptions : TypeSafeItemSelectorOptionProducts =
-            TypeSafeItemSelectorOptionProducts(MiamNeutralItemSelectorOptionProducts())
-        
-    ){
+            TypeSafeItemSelectorOptionProducts(MiamNeutralItemSelectorOptionProducts()),
+        empty: TypeSafeEmpty = TypeSafeEmpty(DefaultEmptyView()),
+        loading: TypeSafeLoading = TypeSafeLoading(DefaultLoadingView()),
+        background: TypeSafeBackground = TypeSafeBackground(DefaultBackgroundView())
+    ) {
         self.selectedProduct = selectedProduct
         self.productOptions = productOptions
-        self.onItemSelected = onItemSelected
+        self.background = background
+        self.empty = empty
+        self.loading = loading
     }
 }
