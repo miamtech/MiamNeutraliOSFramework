844ef715ad4a23f96f37d198f609edad3331a43e
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-11-29 10:01:48 -0500

    update catalog toolbar

--- Sources/MiamNeutraliOSFramework/ui/Catalog/MiamNeutralCatalogResultsToolbar.swift
+++ Sources/MiamNeutraliOSFramework/ui/Catalog/MiamNeutralCatalogResultsToolbar.swift
@@ -10,30 +10,25 @@ import MiamIOSFramework
 
 @available(iOS 14, *)
 public struct MiamNeutralCatalogResultsToolbar: CatalogToolbarProtocol {
-    public init () {}
+    let hasPreferences: Bool
+    public init (hasPreferences: Bool = true) {
+        self.hasPreferences = hasPreferences
+    }
     public func content(
         onFiltersTapped: @escaping () -> Void,
         onSearchTapped: @escaping () -> Void,
         onFavoritesTapped: @escaping () -> Void,
         onPreferencesTapped: @escaping () -> Void
     ) -> some View {
-        HStack(spacing: 16.0) {
+        HStack(spacing: Dimension.sharedInstance.xlPadding) {
+            CatalogToolbarSearchButton(onSearchTapped: onSearchTapped)
             Spacer()
-            CatalogToolbarButtonFormat(
-                icon: Image.miamImage(icon: .search),
-                action: onSearchTapped)
-            CatalogToolbarButtonFormat(
-                icon: Image.miamImage(icon: .filters),
-                action: onFiltersTapped)
-            CatalogToolbarButtonFormat(
-                icon: Image.miamImage(icon: .preferences),
-                action: onPreferencesTapped)
-        }.padding(EdgeInsets(
-            top: Dimension.sharedInstance.mlPadding,
-            leading: Dimension.sharedInstance.mlPadding,
-            bottom: 16,
-            trailing: Dimension.sharedInstance.mlPadding))
-        .background(Color.miamColor(.primaryDark))
+            CatalogToolbarButtonFormat(icon:  Image.miamImage(icon: .filters), action: onFiltersTapped)
+            if hasPreferences {
+                CatalogToolbarButtonFormat(icon:  Image.miamImage(icon: .preferences), action: onPreferencesTapped)
+            }
+        }
+        .padding(Dimension.sharedInstance.mlPadding)
     }
 }
 
