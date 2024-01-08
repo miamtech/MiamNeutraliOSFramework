9e86f37eebc2965594a80b712ff7bb68f3d2ee23
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-01-08 13:51:08 +0100

    didi feedback

--- Sources/MiamNeutraliOSFramework/ui/RecipeDetails/MiamNeutralRecipeDetailsTagsView.swift
+++ Sources/MiamNeutraliOSFramework/ui/RecipeDetails/MiamNeutralRecipeDetailsTagsView.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Damien Walerowicz on 03/01/2024.
 //
@@ -21,14 +21,11 @@ public struct MiamNeutralRecipeDetailsTagsView: RecipeDetailsTagsProtocol {
             ForEach(tags, id: \.id) { tag in
                 TagItem {
                     HStack{
-                        Image.miamImage(icon: MiamIOSFramework.MiamIcon(rawValue: tag.picto)!)
+                        Image.mealzIcon(icon:tag.picto)
                         Text(tag.text)
                     }
                 }
             }
-            
-            
-            
         } plus: {
             AnyView(Text("...").padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
                 .background(Capsule().fill(Color.miamColor(.backgroundSecondary))))
@@ -37,40 +34,42 @@ public struct MiamNeutralRecipeDetailsTagsView: RecipeDetailsTagsProtocol {
     }
 }
 
-
+/**
+ A class use to add capsule background of each tags
+*/
 
 @available(iOS 14, *)
 struct TagItem <Content> : View where Content: View {
     let content: () -> Content
-
+    
     init(@ViewBuilder content: @escaping () -> Content) {
-           self.content = content
-       }
-
+        self.content = content
+    }
+    
     var body: some View {
         content()
-            .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12)).miamFontStyle(style: MiamFontStyleProvider().bodyMediumStyle)
+            .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12)).miamFontStyle(style: MiamFontStyleProvider.sharedInstance.bodyMediumStyle)
             .background(Capsule().fill(Color.miamColor(.backgroundSecondary)))
-
-
     }
 }
 
+/**
+ A class used to access all subview of a view   */
 
 @available(iOS 13, *)
 extension TupleView {
     public var getViews: [AnyView] {
         makeArray(from: value)
     }
-
+    
     private struct GenericView {
         let body: Any
-
+        
         var anyView: AnyView? {
             AnyView(_fromValue: body)
         }
     }
-
+    
     private func makeArray<Tuple>(from tuple: Tuple) -> [AnyView] {
         func convert(child: Mirror.Child) -> AnyView? {
             withUnsafeBytes(of: child.value) { ptr -> AnyView? in
@@ -78,13 +77,11 @@ extension TupleView {
                 return binded.first?.anyView
             }
         }
-
+        
         let tupleMirror = Mirror(reflecting: tuple)
         return tupleMirror.children.compactMap(convert)
     }
 }
-
-
 
 @available(iOS 14, *)
 class Manager {
@@ -93,26 +90,32 @@ class Manager {
     var plusView : AnyView?
 }
 
+/**
+ A class implementing the a wrapping list of View
+ if expend is false,only one line is draw with a button '...' to expend.
+ If expend is true, all elements are drawn with line break
+  */
+
 @available(iOS 14, *)
 struct WrappedHStack: View {
     @SwiftUI.State var frame: CGSize = .zero
-
+    
     @inline(__always) private static func getWidth<V: View>(of view: V) -> Double {
         let hostingController = UIHostingController(rootView: view)
         return hostingController.sizeThatFits(in: CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)).width
-
+        
     }
     var manager = Manager()
-
+    
     @SwiftUI.State var expend = false
-
+    
     init<Views>(@ViewBuilder content: @escaping () -> TupleView<Views>, @ViewBuilder plus: @escaping () -> AnyView) {
         manager.content = content().getViews
         manager.plusView = plus()
         var width = CGFloat.zero
         manager.lines.append(Array<AnyView>())
         var index = 0
-
+        
         manager.content.forEach { v in
             if width + WrappedHStack.getWidth(of: v.fixedSize()) > UIScreen.main.bounds.size.width {
                 width = 0
@@ -123,7 +126,7 @@ struct WrappedHStack: View {
             width += WrappedHStack.getWidth(of: v)
         }
     }
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             ForEach(0..<manager.lines.count) { l in
@@ -132,20 +135,16 @@ struct WrappedHStack: View {
                         if( expend == true || (expend == false && l == 0 )) {
                             if(expend == false && l == 0 && manager.lines.count > 1 && index == manager.lines[0].count - 1) {
                                 manager.plusView.onTapGesture {
-                                        self.expend.toggle()
-                                    }
-
+                                    self.expend.toggle()
+                                }
                             }else{
-
                                 manager.lines[l][index]
                             }
                         }
                     }
                 }
             }
-
+            
         }
     }
-
-
 }
